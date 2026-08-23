139b78fa82cc1cc070624cff9135adf409d05f3a
Nivetha Ramesh <nivetharamesh021@gmail.com>
2026-08-24 00:07:09 +0530

    Add loops for printing numbers and calculating sum

--- basics/loops.py
+++ basics/loops.py
@@ -1,1 +1,22 @@
 
+for i in range(1, 11):
+    print(i)
+
+
+print("Even numbers:")
+for i in range(1, 21):
+    if i % 2 == 0:
+        print(i)
+
+
+total = 0
+for i in range(1, 11):
+    total += i
+
+print("Sum:", total)
+
+
+count = 1
+while count <= 5:
+    print("Count:", count)
+    count += 1
