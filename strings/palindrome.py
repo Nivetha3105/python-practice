ac1d4698c8e3335f8b44f2a42142ea8df217fe1e
Nivetha Ramesh <nivetharamesh021@gmail.com>
2026-08-24 23:15:12 +0530

    Add palindrome checking function and test cases

--- strings/palindrome.py
+++ strings/palindrome.py
@@ -1,1 +1,23 @@
 
+
+def is_palindrome(text):
+    text = text.lower().replace(" ", "")
+    return text == text[::-1]
+
+
+words = ["madam", "level", "python", "racecar"]
+
+for word in words:
+    if is_palindrome(word):
+        print(word, "-> Palindrome")
+    else:
+        print(word, "-> Not a palindrome")
+
+
+
+word = "Nivetha"
+
+if is_palindrome(word):
+    print(word, "is a palindrome")
+else:
+    print(word, "is not a palindrome")
