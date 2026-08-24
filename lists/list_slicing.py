cb91cc6a7a6fa08155857fa41f4681e7c667db53
Nivetha Ramesh <nivetharamesh021@gmail.com>
2026-08-24 23:16:44 +0530

    Add list slicing examples in list_slicing.py

--- lists/list_slicing.py
+++ lists/list_slicing.py
@@ -1,1 +1,19 @@
 
+
+numbers = [10, 20, 30, 40, 50, 60, 70, 80]
+
+
+print("First three:", numbers[:3])
+
+print("Last three:", numbers[-3:])
+
+print("Index 2 to 5:", numbers[2:6])
+
+print("Every second element:", numbers[::2])
+
+
+print("Reversed:", numbers[::-1])
+
+
+copy_numbers = numbers[:]
+print("Copied list:", copy_numbers)
