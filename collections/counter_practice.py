cdb9796109b2fe4ca46262e3283dff3ad7b7a965
Nivetha Ramesh <nivetharamesh021@gmail.com>
2026-08-28 00:02:56 +0530

    Add Python Counter practice example

--- collections/counter_practice.py
+++ collections/counter_practice.py
@@ -1,1 +1,17 @@
+# Python Counter Practice
 
+from collections import Counter
+
+numbers = [1, 2, 2, 3, 3, 3, 4, 4, 4, 4]
+
+frequency = Counter(numbers)
+
+print("Frequency:", frequency)
+print("Count of 3:", frequency[3])
+print("Most common:", frequency.most_common(2))
+
+text = "programming"
+
+letter_count = Counter(text)
+
+print("Letter frequency:", letter_count)
