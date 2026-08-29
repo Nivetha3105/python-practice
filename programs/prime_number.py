38a92c0fa9837880fe235014e0a37abab02e7a57
Nivetha Ramesh <nivetharamesh021@gmail.com>
2026-08-29 23:23:54 +0530

    Add prime number checking functionality

---
+++ programs/prime_number.py
@@ -0,0 +1,19 @@
+# Check whether a number is prime
+
+def is_prime(number):
+    if number < 2:
+        return False
+
+    for i in range(2, int(number ** 0.5) + 1):
+        if number % i == 0:
+            return False
+
+    return True
+
+
+number = 29
+
+if is_prime(number):
+    print(number, "is a prime number")
+else:
+    print(number, "is not a prime number")
