e5459c5f5a03e9bafffda9c765f1571a2d1716b2
Nivetha Ramesh <nivetharamesh021@gmail.com>
2026-09-01 23:34:30 +0530

    Add function to calculate sum of digits

---
+++ programs/sum_of_digits.py
@@ -0,0 +1,16 @@
+# Find the sum of digits of a number
+
+def sum_of_digits(number):
+    total = 0
+
+    while number > 0:
+        digit = number % 10
+        total += digit
+        number //= 10
+
+    return total
+
+
+number = 12345
+
+print("Sum of digits:", sum_of_digits(number))
